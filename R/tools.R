dd77371a07ffea2b008f87ede5bf6eb0e53ec440
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-04-20 16:51:38 +0200

    New function prepare_data() to create test and train datasets

--- R/tools.R
+++ R/tools.R
@@ -45,3 +45,37 @@ check_losses <- function(vars, data, resp_var = "staff_rangers_log", PA_var = "P
 
   output[-1, ]
 }
+
+
+
+#' Tool function to compute train and test datasets given a formula
+#'
+#' @param formula the formula for the LMM or RF
+#' @param data the complete dataset
+#' @param test_prop the amount of rows to keep in the test dataset (default = 0)
+#' @export
+#'
+#' @examples
+#' prepare_data(log(staff_rangers + 1) ~ log(GDP_2019), data = data_rangers, test_prop = 0.1)
+#'
+prepare_data <- function(formula, data, test_prop = 0) {
+  if (test_prop < 0 | test_prop > 1) stop("test_prop must be between 0 and 1")
+  vars <- all.vars(formula)
+  if (any(vars == ".")) {
+    vars <- colnames(data)
+  }
+  mf <- stats::model.frame(formula = formula, data = data)
+  row_to_drop <- as.numeric(attr(mf, "na.action"))
+  data_train <- data[-row_to_drop, vars]
+  data_test <- tibble::tibble()
+  if (test_prop > 0) {
+    nrow_test <- ceiling(nrow(data_train)*test_prop)
+    row_test <- sample(seq_len(nrow(data_train)), size = nrow_test, replace = FALSE)
+    data_test <- data_train[row_test, ]
+    data_train <- data_train[-row_test, ]
+  }
+  list(data_train = data_train, data_test = data_test, missing = length(row_to_drop))
+}
+
+
+
