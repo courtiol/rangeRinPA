ffaede925541fb297819eec5f6c0e2cbd95e76e5
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-04-21 20:22:04 +0200

    Better code

--- scripts/test_accuracy.R
+++ scripts/test_accuracy.R
@@ -1,40 +1,53 @@
-#library(rangeRinPA)
+library(rangeRinPA)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(spaMM)
 library(ranger)
 
+rm(list = ls())
 
-## small, not very general function to compute RMSE on test set for LMM
-compute_rmse_lmm <- function(rep = 10, formula, data, ...) {
-  rmse <- numeric(rep)
-  for (rep in seq_len(rep)) {
+## function to compute RMSE on test set for LMM
+compute_rmse_lmm <- function(formula, data, rep = 10, Ncpu = 1, target = "staff_rangers_log", ...) {
+  rmse <- parallel::mclapply(seq_len(rep), function(i) {
     data_list <- prepare_data(formula = formula, data = data, test_prop = 0.1)
     newfit <- spaMM::fitme(formula = formula, data = data_list$data_train, ...)
-    rmse[rep] <- RMSE(predict(newfit, newdata = data_list$data_test)[, 1], data_list$data_test$staff_rangers_log)
-  }
-  rmse
+    RMSE(predict(newfit, newdata = data_list$data_test)[, 1], data_list$data_test[, target, drop = TRUE])
+  }, mc.cores = Ncpu)
+  unlist(rmse)
 }
 
-## small, not very general function to compute RMSE on test set for RF
-compute_rmse_rf <- function(rep = 10, formula, data, ...) {
-  rmse <- numeric(rep)
-  for (rep in seq_len(rep)) {
+
+## function to compute RMSE on test set for RF
+compute_rmse_rf <- function(formula, data, rep = 10, Ncpu = 1, target = "staff_rangers_log", ...) {
+  rmse <- parallel::mclapply(seq_len(rep), function(i) {
     data_list <- prepare_data(formula = formula, data = data, test_prop = 0.1)
     newfit <- ranger::ranger(formula = formula, data = data_list$data_train,  ...)
-    rmse[rep] <- RMSE(predict(newfit, data = data_list$data_test)$predictions, data_list$data_test$staff_rangers_log)
-  }
-  rmse
+    RMSE(predict(newfit, data = data_list$data_test)$predictions, data_list$data_test[, target, drop = TRUE])
+  }, mc.cores = Ncpu)
+  unlist(rmse)
 }
 
 
 ## data for test
 data_rangers %>%
   filter(countryname_eng != "Greenland") %>%
-  drop_na(staff_rangers, PA_area, pop_density, lat, long, area_country, GDP_2019) %>%
-  mutate(across(c(staff_rangers, PA_area, pop_density, area_country, GDP_2019),
-                ~ log(.x + 1), .names = "{col}_log")) -> data_test ## 135 rows
+  drop_na(staff_rangers,
+          pop_density,
+          lat, long,
+          PA_area, area_country, area_forest_pct,
+          GDP_2019, GDP_capita, GDP_growth, unemployment,
+          EVI, SPI, EPI_2020,
+          IUCN_1_4_prop, IUCN_1_2_prop) %>%
+  mutate(across(c(staff_rangers, PA_area, pop_density, area_country, GDP_2019, GDP_capita, unemployment),
+                ~ log(.x + 1), .names = "{col}_log")) %>%
+  select(staff_rangers_log,
+         pop_density_log,
+         lat, long,
+         PA_area_log, area_country_log, area_forest_pct,
+         GDP_2019_log, GDP_capita_log, GDP_growth, unemployment_log,
+         EVI, SPI, EPI_2020,
+         IUCN_1_4_prop, IUCN_1_2_prop) -> data_test ## 120 rows
 
 
 ## let's define the formula
@@ -48,7 +61,14 @@ formls_LMM <- list(
   forml_PA_pop_GDP_coord      = staff_rangers_log ~ 1 + PA_area_log + pop_density_log + GDP_2019_log +  Matern(1|long + lat),
   forml_area                  = staff_rangers_log ~ 1 + area_country_log,
   forml_GDP                   = staff_rangers_log ~ 1 + GDP_2019_log,
-  forml_PA_pop_area_GDP_coord = staff_rangers_log ~ 1 + PA_area_log + pop_density_log + area_country_log + GDP_2019_log + Matern(1|long + lat)
+  forml_PA_pop_area_GDP_coord = staff_rangers_log ~ 1 + PA_area_log + pop_density_log + area_country_log + GDP_2019_log + Matern(1|long + lat),
+  forml_all                   = staff_rangers_log ~ pop_density_log +
+                                                    lat + long +
+                                                    PA_area_log + area_country_log + area_forest_pct +
+                                                    GDP_2019_log + GDP_capita_log + GDP_growth + unemployment_log +
+                                                    EVI + SPI + EPI_2020 +
+                                                    IUCN_1_4_prop + IUCN_1_2_prop +
+                                                    Matern(1|long + lat)
   )
 
 formls_RF <- list(
@@ -61,37 +81,47 @@ formls_RF <- list(
   forml_PA_pop_GDP_coord      = staff_rangers_log ~ 1 + PA_area_log + pop_density_log + GDP_2019_log +  long + lat,
   forml_area                  = staff_rangers_log ~ 1 + area_country_log,
   forml_GDP                   = staff_rangers_log ~ 1 + GDP_2019_log,
-  forml_PA_pop_area_GDP_coord = staff_rangers_log ~ 1 + PA_area_log + pop_density_log + area_country_log + GDP_2019_log + long + lat
+  forml_PA_pop_area_GDP_coord = staff_rangers_log ~ 1 + PA_area_log + pop_density_log + area_country_log + GDP_2019_log + long + lat,
+  forml_all                   = staff_rangers_log ~ .
 )
 
 
 ## compute RMSE on test sets
-n_tests <- 30
-Ncpu <- 2
+n_tests <- 100
+Ncpu <- 100
 
-test_REML <- parallel::mclapply(formls_LMM, function(f) compute_rmse_lmm(rep = n_tests, formula = f, data = data_test,
-                                                                         control.dist = list(dist.method = "Earth"), method = "REML"), mc.cores = Ncpu)
-
-test_ML <- parallel::mclapply(formls_LMM, function(f) compute_rmse_lmm(rep = n_tests, formula = f, data = data_test,
-                                                       control.dist = list(dist.method = "Earth"), method = "ML"), mc.cores = Ncpu)
-
-test_RF_mtry1 <- parallel::mclapply(formls_RF, function(f) compute_rmse_rf(rep = n_tests, formula = f, data = data_test, mtry = 1), mc.cores = Ncpu)
+test_REML       <- lapply(formls_LMM, function(f) compute_rmse_lmm(formula = f, data = data_test, rep = n_tests, Ncpu = Ncpu,
+                                                                   control.dist = list(dist.method = "Earth"), method = "REML"))
+test_ML         <- lapply(formls_LMM, function(f) compute_rmse_lmm(formula = f, data = data_test, rep = n_tests, Ncpu = Ncpu,
+                                                                   control.dist = list(dist.method = "Earth"), method = "ML"))
+test_RF_mtry1   <- lapply(formls_RF, function(f) compute_rmse_rf(formula = f, data = data_test, rep = n_tests, Ncpu = Ncpu,
+                                                                 mtry = 1))
+test_RF_mtry1_20xmoretrees   <- lapply(formls_RF, function(f) compute_rmse_rf(formula = f, data = data_test, rep = n_tests, Ncpu = Ncpu,
+                                                                 mtry = 1, num.trees = 10000))
+test_RF_mtry3   <- lapply(formls_RF, function(f) compute_rmse_rf(formula = f, data = data_test, rep = n_tests, Ncpu = Ncpu,
+                                                                 mtry = function(nv) min(c(nv, 3))))
+test_RF_mtryMAX <- lapply(formls_RF, function(f) compute_rmse_rf(formula = f, data = data_test, rep = n_tests, Ncpu = Ncpu,
+                                                                 mtry = function(nv) nv))
 
 
 ## let's plot the results
-rbind(cbind(do.call("cbind", test_REML), method = "REML" ),
-      cbind(do.call("cbind", test_ML), method = "ML" ),
-      cbind(do.call("cbind", test_RF_mtry1), method = "RF_mtry1" )) %>%
+rbind(cbind(do.call("cbind", test_REML), method = "spaMM_REML" ),
+      cbind(do.call("cbind", test_ML), method = "spaMM_ML" ),
+      cbind(do.call("cbind", test_RF_mtry1), method = "RF_mtry1" ),
+      cbind(do.call("cbind", test_RF_mtry1_20xmoretrees), method = "RF_mtry1_20xmoretrees" ),
+      cbind(do.call("cbind", test_RF_mtry3), method = "RF_mtry3" ),
+      cbind(do.call("cbind", test_RF_mtryMAX), method = "RF_mtryMAX")) %>%
       as.data.frame() %>%
       pivot_longer(cols = -method, names_to = "model", values_to = "rmse") %>%
       mutate(model = sub(pattern = "forml_", replacement = "", model),
              rmse = as.numeric(rmse),
              model = forcats::fct_inorder(model)) -> test_for_plot
 
+pdf("result_accuracy.pdf", width = 15)
 ggplot(test_for_plot) +
   aes(y = rmse, x = model, colour = method, shape = method) +
-  geom_boxplot(width = 0.1, position = position_dodge(width = 0.2)) +
-  geom_point(position = position_dodge(width = 0.5)) +
+  geom_boxplot(width = 0.3, position = position_dodge(width = 0.5)) +
   labs(y = "RMSE (on log + 1 response)", x = "Model", title = "Accuracy on test sets (10% of data)") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45))
+dev.off()
