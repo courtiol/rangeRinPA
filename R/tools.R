2f7410161d487dacabf4a429ea051040a9f836e2
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-04-26 21:37:08 +0200

    New functions compute_distance & compute_metrics

--- R/tools.R
+++ R/tools.R
@@ -1,5 +1,5 @@
 
-#' Tool function to compute missing values and missing PA
+#' Count missing values and missing PA
 #'
 #' @param vars a vector of quoted variable names
 #' @param data a data.frame or tibble
@@ -47,12 +47,12 @@ check_losses <- function(vars, data, resp_var = "staff_rangers_log", PA_var = "P
 }
 
 
-
-#' Tool function to compute train and test datasets given a formula
+#' Create train and test datasets given a formula and data
 #'
 #' @param formula the formula for the LMM or RF
 #' @param data the complete dataset
 #' @param test_prop the amount of rows to keep in the test dataset (default = 0)
+#' @param keep.var a vector of character strings indicating which variables to keep in the dataset on top of those defined by the formula
 #' @param seed an optional seed for the RNG
 #' @export
 #'
@@ -60,13 +60,16 @@ check_losses <- function(vars, data, resp_var = "staff_rangers_log", PA_var = "P
 #' prepare_data(log(staff_rangers + 1) ~ log(GDP_2019) + Matern(1|lat + long),
 #'              data = data_rangers, test_prop = 0.1)
 #'
-prepare_data <- function(formula, data, test_prop = 0, seed = NULL) {
+prepare_data <- function(formula, data, test_prop = 0, keep.var = NULL, seed = NULL) {
   if (test_prop < 0 | test_prop > 1) stop("test_prop must be between 0 and 1")
   vars <- all.vars(formula)
   if (any(vars == ".")) {
     vars <- colnames(data)
   }
   #mf <- stats::model.frame(formula = formula, data = data) ## cannot handle random effects so we do differently
+  if (!is.null(keep.var)) {
+    vars <- unique(c(vars, keep.var))
+  }
   data <- data[, vars]
   omit_obj <- stats::na.omit(data)
   row_to_drop <- as.numeric(attr(omit_obj, "na.action"))
@@ -89,7 +92,7 @@ prepare_data <- function(formula, data, test_prop = 0, seed = NULL) {
 }
 
 
-#' Tool function to identify variables available for focal countries
+#' Identify variables available for focal countries
 #'
 #' @param country a vector of ISO code(s) of the focal country or countries
 #' @param data the complete dataset
@@ -108,13 +111,80 @@ prepare_vars <- function(country, data) {
 globalVariables("where") ## since not properly exported from tidyselect...
 
 
-#' Compute root mean square error
+#' Compare predictions and observations using various metrics
 #'
-#' @param pred a vector of predictions
-#' @param obs a vector of observations
+#' @param pred a vector of predicted values
+#' @param obs a vector of observed values
+#' @param inv.dist a matrix of inverse distance between points (optional but needed to compute Moran's I)
 #'
+#' @return a data frame with all the metrics
 #' @export
 #'
-RMSE <- function(pred, obs) {
-  sqrt(sum((pred - obs)^2))
+#' @examples
+#' compute_metrics(pred = c(1.1, 1.8, 2.5, 4.5),
+#'                 obs = c(1, 2, 3, 4))
+#'
+#' proxy <- compute_distance(long = c(13.41, -0.13, -77.04, 116.24),
+#'                           lat = c(52.52, 51.51, 38.89, 39.90),
+#'                           inv = TRUE)
+#'
+#' compute_metrics(pred = c(1.1, 1.8, 2.5, 4.5),
+#'                 obs = c(1, 2, 3, 4),
+#'                 inv.dist = proxy)
+#'
+compute_metrics <- function(pred, obs, inv.dist = NULL) {
+
+  resid <- obs - pred
+
+  RMSE <- sqrt(sum((pred - obs)^2)) # root mean squared error
+
+  ME   <- mean(resid) # average bias in predictions
+
+  MAE <- mean(abs(resid)) # mean absolute error
+
+  R2 <- 1 - (sum((resid)^2) / sum((obs - mean(obs))^2)) # R2
+
+  RRSE <- sqrt(((sum(pred) - sum(obs))^2) / sum(obs)) # root relative sum error (homemade)
+
+  CCC <- (2*stats::cor(pred, obs)*stats::sd(pred)*stats::sd(pred))/(stats::var(pred) + stats::var(obs) + (mean(pred) - mean(obs))^2)
+  ## Note: CCC = Lin's concordance correlation coef (Steichen & Cox, 2002)
+
+  MoranI <- c(obs = NA, expected = NA, sd = NA, p.value = NA) # Moran's I
+
+  if (!is.null(inv.dist)) {
+    MoranI <- unlist(ape::Moran.I(resid, inv.dist))
+    names(MoranI) <- paste0("MoranI_", names(MoranI))
   }
+  c(c(RMSE = RMSE, ME = ME, MAE = MAE, R2 = R2, RRSE = RRSE, CCC = CCC), MoranI)
+}
+
+
+#' Compute the distance (or inverse distance) between locations
+#'
+#' @param long a vector of longitudes
+#' @param lat a vector of latitudes
+#' @param inv whether to return the inverse of the distance (default = FALSE)
+#'
+#' @return a square data frame
+#' @export
+#'
+#' @examples
+#' # distance Berlin/London:
+#' compute_distance(long = c(13.41, -0.13), lat = c(52.52, 51.51))
+#'
+compute_distance <- function(long, lat, inv = FALSE) {
+
+  dist <- spaMM::make_scaled_dist(data.frame(long = long, lat = lat),
+                                  rho = 1, dist.method = "Earth",
+                                  return_matrix = TRUE)
+  if (inv) {
+    dist <- 1/dist
+    diag(dist) <- 0
+  }
+
+  dist <- as.data.frame(dist)
+  colnames(dist) <- paste0("loc_", seq_len(ncol(dist)))
+  rownames(dist) <- paste0("loc_", seq_len(ncol(dist)))
+
+  dist
+}
