32b57c18969522fab54fa1442822e879c3c7f661
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-04-27 12:42:52 +0200

    Export function for CV/OOB in RF

--- R/crossvalidation.R
+++ R/crossvalidation.R
@@ -39,3 +39,77 @@ crossvalidate_LMM <- function(formula, data, rep = 10, Ncpu = 1, target = "staff
   cbind(CV_test = seq_len(rep), as.data.frame(do.call("rbind", metrics)))
 }
 
+
+
+#' Compute accuracy metrics on test set for RF
+#'
+#' @inheritParams crossvalidate_LMM
+#' @param spatial either FALSE (default) or "dist" (to use matrix of distances as predictor) or "coord" (to use long & lat as predictors)
+#' @param method either "CV" for cross-validation or "OOB" for directly using the out-of-bag observations generated when growing the forest
+#' @param ... additional parameters to be passed to [`ranger::ranger()`]
+#'
+#' @return a tibble with the CV/OOB replicates in row and accuracy metrics in columns
+#' @export
+#'
+#' @seealso [`compute_metrics()`] for details on the function computing the values outputted here
+#'
+#' @examples
+#' crossvalidate_RF(staff_rangers_log ~ PA_area_log, data = data_test, method = "CV")
+#' crossvalidate_RF(staff_rangers_log ~ PA_area_log, data = data_test,
+#'                  rep = 1, num.trees = 10, method = "OOB")
+#'
+#'
+crossvalidate_RF <- function(formula, data, rep = 10, Ncpu = 1, target = "staff_rangers_log", spatial = FALSE, seed = 123, method = "CV", ...) {
+
+  if (spatial == "coord") {
+    formula <- stats::update.formula(formula, . ~ . + lat + long)
+  } else if (spatial == "dist") { ## add distance to each location as predictors
+      data <- cbind(data, compute_distance(long = data$long, lat = data$lat))
+      dist_vars <- colnames(data)[grep("loc_", colnames(data))]
+      formula <- stats::as.formula(paste(formula[[2]], paste(c(formula[[3]], dist_vars), collapse = "+"), sep = " ~ "))
+  } else if (spatial != FALSE) {
+    stop("Spatial method not found")
+  }
+  if (method == "CV") {
+    metrics <- parallel::mclapply(seq_len(rep), function(i) {
+      data_list <- prepare_data(formula = formula, data = data, test_prop = 0.1,
+                                keep.var =  c("long", "lat"), seed = seed + i)
+      newfit <- ranger::ranger(formula = formula, data = data_list$data_train, num.threads = 1, ...)
+      predicted <- stats::predict(newfit, data = data_list$data_test, num.threads = 1)$predictions
+      observed <- data_list$data_test[, target, drop = TRUE]
+      inv.dist <- NULL
+      if (all(c("long", "lat") %in% colnames(data_list$data_test))) {
+        inv.dist <- compute_distance(long = data_list$data_test$long,
+                                     lat = data_list$data_test$lat,
+                                     inv = TRUE)
+      }
+      compute_metrics(predicted, observed, inv.dist = inv.dist)
+      #NOTE: num.threads = 1 is an attempt not to use multiple threads since we do parallelisation at a higher level, but does not seem to work :-(
+    }, mc.cores = Ncpu)
+    return(cbind(CV_test = seq_len(rep), as.data.frame(do.call("rbind", metrics))))
+    } else if (method == "OOB") {
+      if (rep > 1) {
+        warning("Argument rep ignore when method = 'OOB', you can influence repetitions using the argument num.tress passed to ranger() instead")
+      }
+      newfit <- ranger::ranger(formula = formula, data = data, num.threads = 1, keep.inbag = TRUE, ...)
+      inbag <- do.call(cbind, newfit$inbag.counts)
+      preds <- stats::predict(newfit, data = data, predict.all = TRUE, num.threads = 1)$predictions
+      preds <- preds * ifelse(inbag == 0, NA, 1)
+      inv.dist_full <- NULL
+      if (all(c("long", "lat") %in% colnames(data))) {
+        inv.dist_full <- compute_distance(long = data$long,
+                                     lat = data$lat,
+                                     inv = TRUE)
+      }
+      metrics <- lapply(seq_len(ncol(preds)), function(i) {
+        predicted <- preds[, i, drop = TRUE][!is.na(preds[, i])]
+        observed <- data[!is.na(preds[, i]), target, drop = TRUE]
+        inv.dist <- NULL
+        if (all(c("long", "lat") %in% colnames(data))) {
+          inv.dist <- inv.dist_full[!is.na(preds[, i]), !is.na(preds[, i])]
+        }
+        compute_metrics(pred = predicted, obs = observed, inv.dist = inv.dist)
+      })
+      return(cbind(OOB_test = seq_len(ncol(preds)), as.data.frame(do.call("rbind", metrics))))
+    } else stop("method unknown")
+}
