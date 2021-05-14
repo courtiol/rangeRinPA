7415e2e030bfed834506d8107ab6b1587643ea97
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-05-14 09:08:56 +0200

    Add sd on metrics

--- R/fine_tunning_LMM.R
+++ R/fine_tunning_LMM.R
@@ -19,8 +19,12 @@ finetune_LMM <- function(formula, data, rep = 10, Ncpu = 1, seed = 123) {
                            target = as.character(formula)[2],
                            seed = seed,
                            method = "REML")
-  res <- rbind(cbind(method = "ML", aggregate_metrics(ML_res)),
+
+  res_mean <- rbind(cbind(method = "ML", aggregate_metrics(ML_res)),
                cbind(method = "REML", aggregate_metrics(REML_res)))
 
-  list(result = res, best_method = ifelse(which.min(res$RMSE) == 1, "ML", "REML"))
+  res_sd <- rbind(cbind(method = "ML", aggregate_metrics(ML_res, fn = sd)),
+               cbind(method = "REML", aggregate_metrics(REML_res, fn = sd)))
+
+  list(result_mean = res_mean, result_sd = res_sd, best_method = ifelse(which.min(res_mean$RMSE) == 1, "ML", "REML"))
 }
