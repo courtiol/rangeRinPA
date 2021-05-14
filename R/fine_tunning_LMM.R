5fdfbb242ea43cde97c08c788c34ade051a4d5c3
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-05-14 13:35:14 +0200

    Add missing namespace

--- R/fine_tunning_LMM.R
+++ R/fine_tunning_LMM.R
@@ -23,8 +23,8 @@ finetune_LMM <- function(formula, data, rep = 10, Ncpu = 1, seed = 123) {
   res_mean <- rbind(cbind(method = "ML", aggregate_metrics(ML_res)),
                cbind(method = "REML", aggregate_metrics(REML_res)))
 
-  res_sd <- rbind(cbind(method = "ML", aggregate_metrics(ML_res, fn = sd)),
-               cbind(method = "REML", aggregate_metrics(REML_res, fn = sd)))
+  res_sd <- rbind(cbind(method = "ML", aggregate_metrics(ML_res, fn = stats::sd)),
+               cbind(method = "REML", aggregate_metrics(REML_res, fn = stats::sd)))
 
   list(result_mean = res_mean, result_sd = res_sd, best_method = ifelse(which.min(res_mean$RMSE) == 1, "ML", "REML"))
 }
