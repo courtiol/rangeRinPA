09f71a3ea66eca7d39c29fc0b8b0f53083b92af1
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-05-19 16:34:22 +0200

    Continue workflow

--- scripts/RF_rangers.R
+++ scripts/RF_rangers.R
@@ -1,16 +1,18 @@
 library(rangeRinPA)
 data <- data_rangers
-Ncpu = 2
+Ncpu = 100
 coef = 0
-rep_feature_select = 10
+rep_feature_select = 100
 rep_finetune = 100
 rep_simu = 100
 n_trees = 1000
+rerank = TRUE
 
 data <- fill_PA_area(data, coef = coef)
 
 record <- list(meta = tibble::tibble(start = Sys.time(),
                                      Ncpu = Ncpu,
+                                     rerank = rerank,
                                      rep_feature_select = rep_feature_select,
                                      rep_finetune = rep_finetune,
                                      rep_simu = rep_simu,
@@ -68,9 +70,77 @@ cat("Step 3a: selection for rangers\n")
 selection_training_rangers <- feature_selection_RF(
   full_fit = fit_data_initial_training_rangers_full,
   data = data_initial_training_rangers,
+  rerank = rerank,
   rep = rep_feature_select, Ncpu = Ncpu,
   target = "staff_rangers_log",
   splitrule = "extratrees", replace = FALSE,
   mtry = function(n) n, min.node.size = 1, sample.fraction = 1,
   num.trees = n_trees,
   importance = "impurity")
+
+cat("Step 3b: selection for others\n")
+selection_training_others <- feature_selection_RF(
+  full_fit = fit_data_initial_training_others_full,
+  data = data_initial_training_others,
+  rerank = rerank,
+  rep = rep_feature_select, Ncpu = Ncpu,
+  target = "staff_others_log",
+  splitrule = "extratrees", replace = FALSE,
+  mtry = function(n) n, min.node.size = 1, sample.fraction = 1,
+  num.trees = n_trees,
+  importance = "impurity")
+
+cat("Step 3c: selection for all\n")
+selection_training_all <- feature_selection_RF(
+  full_fit = fit_data_initial_training_all_full,
+  data = data_initial_training_all,
+  rerank = rerank,
+  rep = rep_feature_select, Ncpu = Ncpu,
+  target = "staff_total_log",
+  splitrule = "extratrees", replace = FALSE,
+  mtry = function(n) n, min.node.size = 1, sample.fraction = 1,
+  num.trees = n_trees,
+  importance = "impurity")
+
+selected_formula_rangers <- stats::as.formula(selection_training_rangers$formula[1])
+selected_formula_others <- stats::as.formula(selection_training_others$formula[1])
+selected_formula_all <- stats::as.formula(selection_training_all$formula[1])
+
+record$rangers$selected_features <- list(selection_training_rangers)
+record$rangers$selected_formula <- deparse(selected_formula_rangers, width.cutoff = 500)
+record$rangers$selected_spatial <- selection_training_rangers$spatial[1]
+
+record$others$selected_features <- list(selection_training_others)
+record$others$selected_formula <- deparse(selected_formula_others, width.cutoff = 500)
+record$others$selected_spatial <- selection_training_others$spatial[1]
+
+record$all$selected_features <- list(selection_training_all)
+record$all$selected_formula <- deparse(selected_formula_all, width.cutoff = 500)
+record$all$selected_spatial <- selection_training_all$spatial[1]
+
+
+cat("Step 4: Preparation of final training datasets\n")
+
+data_final_training_rangers <- build_final_training_data(data = data,
+                                                         formula = selected_formula_rangers,
+                                                         survey = "complete_known",
+                                                         spatial = record$rangers$selected_spatial)
+
+data_final_training_others <- build_final_training_data(data = data,
+                                                        formula = selected_formula_others,
+                                                        survey = "complete_known",
+                                                        spatial = record$others$selected_spatial)
+
+data_final_training_all <- build_final_training_data(data = data,
+                                                     formula = selected_formula_all,
+                                                     survey = "complete_known",
+                                                     spatial = record$all$selected_spatial)
+
+record$rangers$final_training_nrow <- nrow(data_final_training_rangers)
+record$others$final_training_nrow <- nrow(data_final_training_others)
+record$all$final_training_nrow <- nrow(data_final_training_all)
+record$rangers$final_training_ncol <- ncol(data_final_training_rangers)
+record$others$final_training_ncol <- ncol(data_final_training_others)
+record$all$final_training_ncol <- ncol(data_final_training_all)
+
+cat("Step 5: Selection of function inputs (fine tuning)\n")
