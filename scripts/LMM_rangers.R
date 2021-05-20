dec59642e3893b8b2bd424999b26e5c2aa411d2a
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-05-20 17:01:27 +0200

    Global Ncpu definition

--- scripts/LMM_rangers.R
+++ scripts/LMM_rangers.R
@@ -1,21 +1,22 @@
 library(rangeRinPA)
 
 options(width = 350)
+Ncpu <- 120
 
 ## Run all LMM workflows and save results:
-LMM_000 <- run_LMM_workflow(data = data_rangers, Ncpu = 100, coef = 0)
+LMM_000 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0)
 save(LMM_000, file = "LMM_000.Rdata")
 
-LMM_025 <- run_LMM_workflow(data = data_rangers, Ncpu = 100, coef = 0.25)
+LMM_025 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.25)
 save(LMM_025, file = "LMM_025.Rdata")
 
-LMM_050 <- run_LMM_workflow(data = data_rangers, Ncpu = 100, coef = 0.50)
+LMM_050 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.50)
 save(LMM_050, file = "LMM_050.Rdata")
 
-LMM_075 <- run_LMM_workflow(data = data_rangers, Ncpu = 100, coef = 0.75)
+LMM_075 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.75)
 save(LMM_075, file = "LMM_075.Rdata")
 
-LMM_100 <- run_LMM_workflow(data = data_rangers, Ncpu = 100, coef = 1)
+LMM_100 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 1)
 save(LMM_100, file = "LMM_100.Rdata")
 
 ## Load results from LMM workflows:
--- scripts/RF_rangers.R
+++ scripts/RF_rangers.R
@@ -1,21 +1,22 @@
 library(rangeRinPA)
 
 options(width = 350)
+Ncpu <- 120
 
 ## Run all RF workflows and save results:
-RF_000 <- run_RF_workflow(data = data_rangers, Ncpu = 100, coef = 0)
+RF_000 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0)
 save(RF_000, file = "RF_000.Rdata")
 
-RF_025 <- run_RF_workflow(data = data_rangers, Ncpu = 100, coef = 0.25)
+RF_025 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.25)
 save(RF_025, file = "RF_025.Rdata")
 
-RF_050 <- run_RF_workflow(data = data_rangers, Ncpu = 100, coef = 0.50)
+RF_050 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.50)
 save(RF_050, file = "RF_050.Rdata")
 
-RF_075 <- run_RF_workflow(data = data_rangers, Ncpu = 100, coef = 0.75)
+RF_075 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.75)
 save(RF_075, file = "RF_075.Rdata")
 
-RF_100 <- run_RF_workflow(data = data_rangers, Ncpu = 100, coef = 1)
+RF_100 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 1)
 save(RF_100, file = "RF_100.Rdata")
 
 ## Load results from RF workflows:
