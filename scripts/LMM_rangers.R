271898b6994954affff23672ec1bc32644dc8458
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-06-09 15:11:27 +0200

    Save results in correct location

--- scripts/LMM_rangers.R
+++ scripts/LMM_rangers.R
@@ -5,16 +5,16 @@ Ncpu <- 120
 
 ## Run all LMM workflows and save results:
 LMM_000 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0)
-save(LMM_000, file = "LMM_000.Rdata")
+save(LMM_000, file = "./scripts/results/LMM_000.Rdata")
 
 LMM_025 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.25)
-save(LMM_025, file = "LMM_025.Rdata")
+save(LMM_025, file = "./scripts/results/LMM_025.Rdata")
 
 LMM_050 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.50)
-save(LMM_050, file = "LMM_050.Rdata")
+save(LMM_050, file = "./scripts/results/LMM_050.Rdata")
 
 LMM_075 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.75)
-save(LMM_075, file = "LMM_075.Rdata")
+save(LMM_075, file = "./scripts/results/LMM_075.Rdata")
 
 LMM_100 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 1)
-save(LMM_100, file = "LMM_100.Rdata")
+save(LMM_100, file = "./scripts/results/LMM_100.Rdata")
--- scripts/RF_rangers.R
+++ scripts/RF_rangers.R
@@ -5,16 +5,16 @@ Ncpu <- 120
 
 ## Run all RF workflows and save results:
 RF_000 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0)
-save(RF_000, file = "RF_000.Rdata")
+save(RF_000, file = "./scripts/results/RF_000.Rdata")
 
 RF_025 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.25)
-save(RF_025, file = "RF_025.Rdata")
+save(RF_025, file = "./scripts/results/RF_025.Rdata")
 
 RF_050 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.50)
-save(RF_050, file = "RF_050.Rdata")
+save(RF_050, file = "./scripts/results/RF_050.Rdata")
 
 RF_075 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.75)
-save(RF_075, file = "RF_075.Rdata")
+save(RF_075, file = "./scripts/results/RF_075.Rdata")
 
 RF_100 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 1)
-save(RF_100, file = "RF_100.Rdata")
+save(RF_100, file = "./scripts/results/RF_100.Rdata")
