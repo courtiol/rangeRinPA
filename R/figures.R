ab46b71556cffd4484f801a0e8c3c17d6881daf9
courtiol <alexandre.courtiol@gmail.com>
2021-07-29 13:24:40 +0200

    Sampling desing figure now in function

---
+++ R/figures.R
@@ -0,0 +1,84 @@
+
+#' Plot the map of the sampling design
+#'
+#' @param data the complete dataset
+#' @param proj the projection to be used (e.g. "+proj=moll" - the default)
+#'
+#' @export
+#'
+#' @examples
+#' plot_map_sampling(data_rangers)
+#'
+#' if(require(patchwork)) {
+#'   plot_map_sampling(data_rangers, proj = "+proj=moll") +
+#'   plot_map_sampling(data_rangers, proj = "+proj=robin" ) +
+#'   plot_map_sampling(data_rangers, proj = "+proj=wintri") +
+#'   plot_map_sampling(data_rangers, proj = "+proj=natearth2") +
+#'   plot_map_sampling(data_rangers, proj = "+proj=mbt_fps") +
+#'   plot_map_sampling(data_rangers, proj = "+proj=hammer") +
+#'   plot_map_sampling(data_rangers, proj = "+proj=wag1") +
+#'   plot_map_sampling(data_rangers, proj = "+proj=goode") +
+#'   plot_map_sampling(data_rangers, proj = "+proj=eqearth") +
+#'   plot_map_sampling(data_rangers, proj = "+proj=eck4") +
+#'   plot_map_sampling(data_rangers, proj = "+proj=boggs") +
+#'   plot_map_sampling(data_rangers, proj = "+proj=aitoff")
+#' }
+#'
+plot_map_sampling <- function(data, proj = "+proj=moll") {
+
+  ## obtain sf all all countries:
+  world_sf <- rnaturalearth::ne_countries(scale = "medium", returnclass = "sf")
+
+  ## add prefix to variable others than geometry:
+  i <- which(names(world_sf) != "geometry")
+  names(world_sf)[i] <- paste0("rne_", names(world_sf)[i])
+
+  ## manual fix of some iso codes discrepancies:
+  world_sf$rne_iso_a3[world_sf$rne_name == "Kosovo"] <- "KOS"
+
+  ## check locations not found in map (depend on scale defined above):
+  data |>
+    dplyr::anti_join(world_sf, by = c(countryname_iso = "rne_iso_a3")) |>
+    dplyr::pull(.data$countryname_eng) -> missing
+
+  if (length(missing) > 0) {
+    cat("Here are the countries/territories for which polygons are not found:\n")
+    print(missing)
+  }
+
+  ## only keep locations found in map:
+  data |>
+    dplyr::right_join(world_sf, by = c(countryname_iso = "rne_iso_a3")) |>
+    sf::st_as_sf() -> world_rangers
+
+  world_rangers |>
+    dplyr::mutate(PA_area_surveyed = ifelse(.data$PA_area_surveyed == 0, NA, .data$PA_area_surveyed),
+                  PA_area_surveyed2 = cut(PA_area_surveyed, breaks = seq(0, 100, 20))) -> world_rangers
+
+  ## applying projection:
+  world_rangers |>
+    sf::st_transform(crs = proj) -> world_rangers
+
+  ## computing variable of interest:
+  world_rangers |>
+    dplyr::mutate(sampled_coverage = 100*.data$PA_area_surveyed / (.data$PA_area_surveyed + .data$PA_area_unsurveyed)) -> world_rangers
+
+  ## plotting:
+  ggplot2::ggplot() +
+    ggplot2::geom_sf(mapping = ggplot2::aes(fill = sampled_coverage),
+                     data = world_rangers, colour = "black", size = 0.05) +
+    ggplot2::scale_fill_fermenter(palette = 2,
+                                  direction = 1,
+                                  breaks = seq(0, 80, 20),
+                                  na.value = "orange",
+                                  guide = ggplot2::guide_colorsteps(title = "Protected areas\n sampled (%)",
+                                                                    title.vjust = 1, barwidth = 1.5,
+                                                                    label.theme = ggplot2::element_text(angle = 0),
+                                                                    label.hjust = 0, label.vjust = 0.5)) +
+    ggplot2::theme_void() +
+    ggplot2::theme(legend.position = "left",
+                   #panel.grid = ggplot2::element_line(colour = "GREY", size = 0.3),
+                   panel.grid = ggplot2::element_blank()) +
+    ggplot2::coord_sf(expand = FALSE, crs = proj)
+
+  }
--- scripts/Process_results.R
+++ scripts/Process_results.R
@@ -269,43 +269,9 @@ ggsave("./scripts/figures/predictor_area_country.png", width = 13, height = 9, s
 
 # Map of the sampling
 
-world_sf <- ne_countries(scale = "medium", returnclass = "sf")
-i <- which(names(world_sf) != "geometry")
-names(world_sf)[i] <- paste0("rne_", names(world_sf)[i])
-
-## check locations not found in map (depend on scale defined above):
-data_rangers %>%
- anti_join(world_sf, by = c(countryname_iso = "rne_iso_a3")) %>%
- pull(countryname_eng)
-
-## only keep locations found in map:
-data_rangers %>%
- right_join(world_sf, by = c(countryname_iso = "rne_iso_a3")) %>%
- st_as_sf() -> world_rangers
-
-world_rangers %>%
-  mutate(PA_area_surveyed = ifelse(PA_area_surveyed == 0, NA, PA_area_surveyed)) -> world_rangers
-
-## applying projection:
-world_rangers %>%
- st_transform(crs = "+proj=moll") -> world_rangers
-
-ggplot() +
-  geom_sf(mapping = aes(fill = PA_area_surveyed / (PA_area_surveyed + PA_area_unsurveyed)),
-          data = world_rangers, colour = "black", size = 0.05) +
-  scale_fill_fermenter(palette = 2,
-                       direction = 1,
-                       breaks = seq(0, 0.8, 0.2),
-                       guide = guide_colorsteps(title = "Proportion of PA area surveyed",
-                                                title.vjust = 1, barwidth = 10,
-                                                label.theme = element_text(angle = 0),
-                                                label.hjust = 0.5, label.vjust = 1)) +
-  scale_x_continuous(breaks = seq(-180, 180, 30)) +
-  theme_minimal() +
-  theme(legend.position = "bottom", panel.grid = element_line(colour = "GREY", size = 0.3),
-        plot.title = element_text(size = 20, hjust = 0.5))
-ggsave("./scripts/figures/design.pdf", width = 13, height = 9, scale = 0.7)
-ggsave("./scripts/figures/design.png", width = 13, height = 9, scale = 0.7)
+plot_map_sampling(data_rangers)
+ggsave("./scripts/figures/design.pdf", width = 12.5, height = 5.5, scale = 0.7)
+ggsave("./scripts/figures/design.png", width = 12.5, height = 5.5, scale = 0.7)
 
 
 ## Exploration density rangers
@@ -314,7 +280,7 @@ data_rangers |>
   fill_PA_area(coef = 1) |>
   mutate(km2_per_staff = area_PA_total / staff_total) |>
   filter(!is.na(km2_per_staff)) |>
-  select(countryname_iso, countryname_eng, km2_per_staff, country_UN_continent, area_country, area_PA_total) |>
+  select(countryname_iso, countryname_eng, km2_per_staff, staff_total, country_UN_continent, area_country, area_PA_total) |>
   arrange(km2_per_staff) -> d
 
 order_continents <- rev(c("World", "Latin America \n& Caribbean", "Africa", "Oceania", "Asia", "Europe", "Northern\n America")) #\n (incl. Greenland)
