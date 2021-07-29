ce4199b4b08f3110cd5191636807d37d8f6045c7
courtiol <alexandre.courtiol@gmail.com>
2021-07-29 14:47:55 +0200

    Improve legend (related to ggplot2 issue #4567)

--- R/figures.R
+++ R/figures.R
@@ -52,8 +52,7 @@ plot_map_sampling <- function(data, proj = "+proj=moll") {
     sf::st_as_sf() -> world_rangers
 
   world_rangers |>
-    dplyr::mutate(PA_area_surveyed = ifelse(.data$PA_area_surveyed == 0, NA, .data$PA_area_surveyed),
-                  PA_area_surveyed2 = cut(.data$PA_area_surveyed, breaks = seq(0, 100, 20))) -> world_rangers
+    dplyr::mutate(PA_area_surveyed = ifelse(.data$PA_area_surveyed == 0, NA, .data$PA_area_surveyed)) -> world_rangers
 
   ## applying projection:
   world_rangers |>
@@ -61,20 +60,27 @@ plot_map_sampling <- function(data, proj = "+proj=moll") {
 
   ## computing variable of interest:
   world_rangers |>
-    dplyr::mutate(sampled_coverage = 100*.data$PA_area_surveyed / (.data$PA_area_surveyed + .data$PA_area_unsurveyed)) -> world_rangers
+    dplyr::mutate(sampled_coverage = 100*.data$PA_area_surveyed / (.data$PA_area_surveyed + .data$PA_area_unsurveyed),
+                  sampled_coverage2 = cut(.data$sampled_coverage, breaks = seq(0, 100, 20),
+                                          labels = c(paste0(floor(min(.data$sampled_coverage, na.rm = TRUE)), "-20"), "20-40", "40-60", "60-80", "80-100")),
+                  sampled_coverage2 = forcats::fct_rev(.data$sampled_coverage2)) -> world_rangers
 
   ## plotting:
   ggplot2::ggplot() +
-    ggplot2::geom_sf(mapping = ggplot2::aes(fill = .data$sampled_coverage),
+    ggplot2::geom_sf(mapping = ggplot2::aes(fill = .data$sampled_coverage2),
                      data = world_rangers, colour = "black", size = 0.05) +
-    ggplot2::scale_fill_fermenter(palette = 2,
-                                  direction = 1,
-                                  breaks = seq(0, 80, 20),
-                                  na.value = "orange",
-                                  guide = ggplot2::guide_colorsteps(title = "Protected areas\n sampled (%)",
-                                                                    title.vjust = 1, barwidth = 1.5,
-                                                                    label.theme = ggplot2::element_text(angle = 0),
-                                                                    label.hjust = 0, label.vjust = 0.5)) +
+    #ggplot2::scale_fill_fermenter(palette = 2, ## for use with sampled_coverage not sampled_coverage2
+    #                              direction = 1,
+    #                              breaks = seq(0, 80, 20),
+    #                              na.value = "orange",
+    #                              guide = ggplot2::guide_colorsteps(title = "Protected areas\n sampled (%)",
+    #                                                                title.vjust = 1, barwidth = 1.5,
+    #                                                                label.theme = ggplot2::element_text(angle = 0),
+    #                                                                label.hjust = 0, label.vjust = 0.5)) +
+    ggplot2::scale_fill_manual(values = scales::brewer_pal(type = "seq", palette = 2, direction = -1)(length(unique(world_rangers$sampled_coverage2))),
+                               labels = c(levels(world_rangers$sampled_coverage2), "0 (not sampled)"),
+                               na.value = "orange",
+                               guide = ggplot2::guide_legend(title = "Protected areas\n sampled (%)")) +
     ggplot2::theme_void() +
     ggplot2::theme(legend.position = "left",
                    #panel.grid = ggplot2::element_line(colour = "GREY", size = 0.3),
