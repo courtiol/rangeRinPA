db39d005c08074bfef0905b17ea111a385c0368f
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-08-26 23:26:01 +0200

    Edits for real workflow

--- R/rangeRinPA-package.R
+++ R/rangeRinPA-package.R
@@ -17,6 +17,8 @@
 #' @examples
 #' \dontrun{
 #'
+#' library(sf) ## to handle the geometry column in the data
+#'
 #' ## Set and create all directories to store files
 #'
 #' path <- "inst/extdata/" ## set the path where you want to store all created files
@@ -91,44 +93,44 @@
 #'
 #' ### Predictions
 #'
-#' Ncpu <- 2L
-#' # Ncpu <- 120 ## define the number of CPUs to use
+#' #Ncpu <- 2L
+#' Ncpu <- 120 ## define the number of CPUs to use
 #'
 #'
 #' ## Run all RF workflows and save results
 #'
-#' RF_000 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0, rep_feature_select = 2, rep_finetune = 2, rep_simu = 2)
-#' save(RF_000, file = paste0(path_predictions, "RF_000.Rdata"))
-#'
-#' RF_025 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.25, rep_feature_select = 2, rep_finetune = 2, rep_simu = 2)
-#' save(RF_025, file = paste0(path_predictions, "RF_025.Rdata"))
-#'
-#' RF_050 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.50, rep_feature_select = 2, rep_finetune = 2, rep_simu = 2)
-#' save(RF_050, file = paste0(path_predictions, "RF_050.Rdata"))
-#'
-#' RF_075 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.75, rep_feature_select = 2, rep_finetune = 2, rep_simu = 2)
-#' save(RF_075,file = paste0(path_predictions, "RF_075.Rdata"))
-#'
-#' RF_100 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 1, rep_feature_select = 2, rep_finetune = 2, rep_simu = 2)
-#' save(RF_100, file = paste0(path_predictions, "RF_100.Rdata"))
-#'
-#'
-#' ## Run all RF workflows and save results
-#'
-#' LMM_000 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0, rep_feature_select = 2, rep_finetune = 2, rep_simu = 2)
+#' LMM_000 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0)
 #' save(LMM_000, file = paste0(path_predictions, "LMM_000.Rdata"))
 #'
-#' LMM_025 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.25, rep_feature_select = 2, rep_finetune = 2, rep_simu = 2)
+#' LMM_025 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.25)
 #' save(LMM_025, file = paste0(path_predictions, "LMM_025.Rdata"))
 #'
-#' LMM_050 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.50, rep_feature_select = 2, rep_finetune = 2, rep_simu = 2)
+#' LMM_050 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.50)
 #' save(LMM_050, file = paste0(path_predictions, "LMM_050.Rdata"))
 #'
-#' LMM_075 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.75, rep_feature_select = 2, rep_finetune = 2, rep_simu = 2)
+#' LMM_075 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.75)
 #' save(LMM_075, file = paste0(path_predictions, "LMM_075.Rdata"))
 #'
-#' LMM_100 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 1, rep_feature_select = 2, rep_finetune = 2, rep_simu = 2)
+#' LMM_100 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 1)
 #' save(LMM_100, file = paste0(path_predictions, "LMM_100.Rdata"))
+#'
+#'
+#' ## Run all RF workflows and save results
+#'
+#' RF_000 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0)
+#' save(RF_000, file = paste0(path_predictions, "RF_000.Rdata"))
+#'
+#' RF_025 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.25)
+#' save(RF_025, file = paste0(path_predictions, "RF_025.Rdata"))
+#'
+#' RF_050 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.50)
+#' save(RF_050, file = paste0(path_predictions, "RF_050.Rdata"))
+#'
+#' RF_075 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.75)
+#' save(RF_075,file = paste0(path_predictions, "RF_075.Rdata"))
+#'
+#' RF_100 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 1)
+#' save(RF_100, file = paste0(path_predictions, "RF_100.Rdata"))
 #'
 #'
 #' ## Load all results saved on disk (useful only if all objects created above are not already in memory)
