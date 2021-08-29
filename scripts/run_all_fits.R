b5da0af19912b28d269901ed4b066b330b011901
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-08-29 08:59:57 +0200

    Reduce Ncpu for memory not to max out

--- scripts/run_all_fits.R
+++ scripts/run_all_fits.R
@@ -15,36 +15,56 @@ path_predictions <- paste0(path, "predictions/")
 if (!dir.exists(path_predictions)) dir.create(path_predictions)
 
 
-Ncpu <- 120
+Ncpu <- 100
 
 LMM_100 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 1)
 save(LMM_100, file = paste0(path_predictions, "LMM_100.Rdata"))
+rm(LMM_100)
+gc()
 
 LMM_075 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.75)
 save(LMM_075, file = paste0(path_predictions, "LMM_075.Rdata"))
+rm(LMM_075)
+gc()
 
 LMM_050 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.50)
 save(LMM_050, file = paste0(path_predictions, "LMM_050.Rdata"))
+rm(LMM_050)
+gc()
 
 LMM_025 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.25)
 save(LMM_025, file = paste0(path_predictions, "LMM_025.Rdata"))
+rm(LMM_025)
+gc()
 
 LMM_000 <- run_LMM_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0)
 save(LMM_000, file = paste0(path_predictions, "LMM_000.Rdata"))
+rm(LMM_000)
+gc()
 
+n_trees <- 2000
 
+RF_100 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 1, n_trees = n_trees)
+save(RF_100, file = paste0(path_predictions, paste0("RF_100_", n_trees, ".Rdata")))
+rm(RF_100)
+gc()
 
-RF_100 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 1)
-save(RF_100, file = paste0(path_predictions, "RF_100.Rdata"))
+RF_075 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.75, n_trees = n_trees)
+save(RF_075,file = paste0(path_predictions, paste0("RF_075_", n_trees, ".Rdata")))
+rm(RF_075)
+gc()
 
-RF_075 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.75)
-save(RF_075,file = paste0(path_predictions, "RF_075.Rdata"))
+RF_050 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.50, n_trees = n_trees)
+save(RF_050, file = paste0(path_predictions, paste0("RF_050_", n_trees, ".Rdata")))
+rm(RF_050)
+gc()
 
-RF_050 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.50)
-save(RF_050, file = paste0(path_predictions, "RF_050.Rdata"))
+RF_025 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.25, n_trees = n_trees)
+save(RF_025, file = paste0(path_predictions, paste0("RF_025_", n_trees, ".Rdata")))
+rm(RF_025)
+gc()
 
-RF_025 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0.25)
-save(RF_025, file = paste0(path_predictions, "RF_025.Rdata"))
-
-RF_000 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0)
-save(RF_000, file = paste0(path_predictions, "RF_000.Rdata"))
+RF_000 <- run_RF_workflow(data = data_rangers, Ncpu = Ncpu, coef = 0, n_trees = n_trees)
+save(RF_000, file = paste0(path_predictions, paste0("RF_000_", n_trees, ".Rdata")))
+rm(RF_000)
+gc()
